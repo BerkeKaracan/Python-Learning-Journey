9e03da17e28c5c5d7b529d8692f286da821d3e51
BerkeKaracan <berkekaracan1113@gmail.com>
2026-02-07 07:09:11 +0300

    New release: Project 19 : Weather App V1

---
+++ Project-19-WeatherAppv1/README.md
@@ -0,0 +1,42 @@
+# 🌦️ Python Weather App
+
+A lightweight desktop application that fetches real-time weather data for any city using the OpenWeatherMap API. Built with Python and Tkinter.
+
+## 🖼️ Features
+
+- **Real-Time Data:** Fetches live temperature and weather conditions.
+- **GUI Interface:** Simple and easy-to-use window design.
+- **Smart Error Handling:** Warns the user if the city is not found or if there is no internet connection.
+
+## 🛠️ Requirements
+
+You need Python installed and the `requests` library.
+
+To install the required library, run:
+
+    pip install requests
+
+## 🎮 How to Use
+
+1.  **Run the App:**
+
+    ```bash
+    python main.py
+    ```
+
+2.  **Enter Location:**
+    Type a city name (e.g., "London", "Ankara", "Tokyo") into the box.
+
+3.  **Get Results:**
+    Click the **"Enter"** button to see the current temperature and weather status.
+
+## 📂 Project Structure
+
+    ├── main.py          # Source code
+    ├── requirements.txt # Dependencies
+    ├── README.md        # Documentation
+    └── .gitignore       # System files to ignore
+
+---
+
+_Powered by OpenWeatherMap API_ 🌍
---
+++ Project-19-WeatherAppv1/main.py
@@ -0,0 +1,38 @@
+import requests
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Weather Situation")
+window.geometry("300x250")
+title= tk.Label(window, text="Enter location: ")
+title.pack(pady=10)
+location= tk.Entry(window)
+location.pack(pady=5)
+result = tk.Label(window, text="Waiting for location...")
+result.pack(pady=15)
+
+def main():
+    current_location = location.get()
+    if not current_location:
+        return
+    
+    api_key = "8789b64d6527c6498e8d8d72ff53a703"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={current_location}&appid={api_key}&units=metric"
+
+    try:
+        request1 = requests.get(url)
+        if request1.status_code == 200:
+            data = request1.json()
+            temp = data["main"]["temp"]
+            weather = data["weather"][0]["description"]
+            result.config(text= f"Temperature: {temp} Celsius \n Situation: {weather}")
+        else:
+            result.config(text= "Location not found.")
+        
+    except Exception as e:
+        result.config(text="Network error!")
+
+
+button = tk.Button(window, text="Enter" ,command=main)
+button.pack(pady= 10)
+window.mainloop()
